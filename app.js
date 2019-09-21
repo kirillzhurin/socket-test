3e043aa445eaba9ff721311e98b66d3c141924f2
Kirill Zhurin <kirillzhurin@gmail.com>
2019-09-22 02:54:42 +0300

    created connection socket io

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', socket => {
+  console.log('a user connected');
+})
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server has been started on ${port}`));
+http.listen(port, () => console.log(`Server has been started on ${port}`));
