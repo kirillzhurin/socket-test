3209f00b05d411e444618908a900dae43aefa938
Kirill Zhurin <kirillzhurin@gmail.com>
2019-09-22 02:24:51 +0300

    added index page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 const port = process.env.PORT || 3000;
