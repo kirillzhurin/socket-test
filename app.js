c8ad6c8b80d21de1f50f446abbe4cce2e51a066c
Kirill Zhurin <kirillzhurin@gmail.com>
2019-09-23 01:12:15 +0300

    simple chat

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const app = require('express')();
-const http = require('http').Server(app);
+const http = require('http').Server(app);       
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
@@ -8,6 +8,15 @@ app.get('/', (req, res) => {
 
 io.on('connection', socket => {
   console.log('a user connected');
+  
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+  });
+
+  socket.on('chat message', msg => {
+    console.log('message:', msg);
+    socket.broadcast.emit('chat message', msg);
+  });
 })
 const port = process.env.PORT || 3000;
 http.listen(port, () => console.log(`Server has been started on ${port}`));
